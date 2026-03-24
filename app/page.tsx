2a8d69cf8e989fb1a42b49bcad7947648618cca7
Deepanshu Sharma <deepanshusharma12098@ce.du.ac.in>
2026-03-24 23:31:19 +0530

    Bootstrapped Next.js application using default configuration and initial project scaffolding

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div className='min-h-screen flex items-center justify-center'>
+    <h1 className="text-5xl text-center font-bold">
+      starting the project --- complaint management system
+    </h1>
+   </div>
+  );
+}
